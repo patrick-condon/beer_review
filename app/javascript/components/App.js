cb831b07c9ad931247002be842491b5585e7cd6c
Pat Condon <condonpatrickj@gmail.com>
2018-03-12 11:50:36 -0400

    add react-router

---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Router, browserHistory, Route, IndexRoute } from 'react-router';
+
+const App = props => {
+
+  // return(
+  //   <Router history={browserHistory}>
+  //     <Route path='/'>
+  //     <IndexRoute component={CerealsIndexContainer} />
+  //       <Route path="cereals" component={CerealsIndexContainer} />
+  //       <Route path="cereals/:id" component={CerealShowContainer} />
+  //     </Route>
+  //   </Router>
+  // )
+
+}
+export default App;
