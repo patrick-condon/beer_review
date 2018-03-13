f859ece8305bb235948b44efdbd16c3c2ffec16b
Pat Condon <condonpatrickj@gmail.com>
2018-03-13 11:01:38 -0400

    add edit button to layout

--- spec/features/user_updates_profile_spec.rb
+++ spec/features/user_updates_profile_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'user updates information', %Q{
+feature 'user updates information', %{
   As an authenticated user
   I want to update my information
   So that I can keep my profile up to date
@@ -14,7 +14,6 @@ feature 'user updates information', %Q{
     user = FactoryBot.create(:user)
 
     visit new_user_session_path
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
 
@@ -22,8 +21,7 @@ feature 'user updates information', %Q{
 
     expect(page).to have_content('Signed in successfully')
 
-    visit edit_user_registration_path
-
+    click_on 'Edit'
     fill_in "Password", with: 'password'
     fill_in "Password confirmation", with: 'password'
     fill_in "Current password", with: user.password
@@ -36,52 +34,43 @@ feature 'user updates information', %Q{
     user = FactoryBot.create(:user)
 
     visit new_user_session_path
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-
     click_button 'Log in'
 
     expect(page).to have_content('Signed in successfully')
 
-    visit edit_user_registration_path
-
+    click_on 'Edit'
     fill_in "Password", with: 'newpassword'
     fill_in "Password confirmation", with: 'newpassword'
     fill_in "Current password", with: user.password
-    old_password = user.password
     click_on 'Update'
 
     expect(page).to have_content('Your account has been updated successfully')
 
-    # save_and_open_page
     click_link "Sign Out"
+
     expect(page).to have_content('Signed out successfully')
 
     click_on "Sign In"
     fill_in 'Email', with: user.email
-    fill_in 'Password', with: old_password
-
+    fill_in 'Password', with: user.password
     click_button 'Log in'
 
     expect(page).to have_content('Invalid Email or password')
-
   end
 
   scenario 'user deletes account' do
     user = FactoryBot.create(:user)
 
     visit new_user_session_path
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
-
     click_button 'Log in'
 
     expect(page).to have_content('Signed in successfully')
 
-    visit edit_user_registration_path
-
+    click_on 'Edit'
     click_on 'Cancel my account'
 
     expect(page).to have_content('Your account has been successfully cancelled')
