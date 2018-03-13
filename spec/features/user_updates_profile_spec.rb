566614f14232e005e2e355321c32e11924d3f7a2
Pat Condon <condonpatrickj@gmail.com>
2018-03-13 11:28:26 -0400

    reduce block to 25 lines

--- spec/features/user_updates_profile_spec.rb
+++ spec/features/user_updates_profile_spec.rb
@@ -1,36 +1,13 @@
 require 'rails_helper'
 
 feature 'user updates information' do
-  scenario 'authenticated user edits information' do
-    user = FactoryBot.create(:user)
-
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-
-    click_button 'Log in'
-
-    expect(page).to have_content('Signed in successfully')
-
-    click_on 'Edit'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    fill_in 'Current password', with: user.password
-    click_on 'Update'
-
-    expect(page).to have_content('Your account has been updated successfully')
-  end
-
-  scenario 'edited information persisted' do
+  scenario 'authenticated user edits information that persists' do
     user = FactoryBot.create(:user)
 
     visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Log in'
-
-    expect(page).to have_content('Signed in successfully')
-
     click_on 'Edit'
     fill_in 'Password', with: 'newpassword'
     fill_in 'Password confirmation', with: 'newpassword'
@@ -40,9 +17,6 @@ feature 'user updates information' do
     expect(page).to have_content('Your account has been updated successfully')
 
     click_link 'Sign Out'
-
-    expect(page).to have_content('Signed out successfully')
-
     click_on 'Sign In'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
