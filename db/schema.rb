8ecce6b41ae263754ec39f65831ed2d770ea0970
robert haynes <roberthaynes@roberts-MacBook.local>
2018-03-13 13:32:18 -0400

    added beer table, model and test_8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313140133) do
+ActiveRecord::Schema.define(version: 20180313172809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20180313140133) do
     t.string "brewery_name", null: false
     t.string "beer_style", null: false
     t.float "beer_abv", null: false
-    t.string "beer_description"
+    t.text "beer_description"
     t.integer "beer_active"
     t.string "beer_label"
     t.datetime "created_at", null: false
