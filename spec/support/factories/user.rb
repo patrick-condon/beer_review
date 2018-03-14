6d8aaa214f503b389ad19d5edb087a3306954946
robert haynes <roberthaynes@roberts-MacBook.local>
2018-03-14 11:37:37 -0400

    added review table with tests5

--- spec/support/factories/user.rb
+++ spec/support/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:username) {|n| "user#{n}" }
-    sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:username) { |n| "user#{n}" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
   end
