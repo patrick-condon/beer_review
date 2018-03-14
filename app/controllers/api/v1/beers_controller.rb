b9c5058f29855364a8217c4abdf9d8e530fdcdef
Pat Condon <condonpatrickj@gmail.com>
2018-03-14 15:58:52 -0400

    ignoring codeclimate

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,12 +1,6 @@
 # API backend for Beers Index
-class ApiController
-  class Api
-    class V1
-      class BeersController
-        def index
-          render json: { beers: Beer.all }
-        end
-      end
-    end
+class Api::V1::BeersController < ApiController
+  def index
+    render json: { beers: Beer.all }
   end
 end
