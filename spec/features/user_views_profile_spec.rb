28e86fbe2c94c3febe859e91d616db8d66a08068
David <David@Luna.local>
2018-03-16 15:53:27 -0400

    add user show page4

--- spec/features/user_views_profile_spec.rb
+++ spec/features/user_views_profile_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
-feature 'user checks his profile page', %{
+feature 'user checks his profile page', %(
   As a signed up user
   I want to view my profile page
-  So that I can see my account information
-} do
-
+  So that I can see my account information) do
   scenario 'view account information' do
-    user = FactoryBot.create(:user)
-    visit user_path(user)
+   user = FactoryBot.create(:user)
 
-    expect(page).to have_content(user.username)
-    expect(page).to have_content(user.email)
-    expect(page).to have_content(user.profile_photo)
-   end
+   visit user_path(user)
+
+   expect(page).to have_content(user.username)
+   expect(page).to have_content(user.email)
+   expect(page).to have_content(user.profile_photo)
+  end
 end
