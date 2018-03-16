684647dbe7073112d66f3f25ae6c39731f4f0bc6
David <David@Luna.local>
2018-03-16 15:57:07 -0400

    add user show page6

--- spec/features/user_views_profile_spec.rb
+++ spec/features/user_views_profile_spec.rb
@@ -5,12 +5,12 @@ feature 'user checks his profile page', %(
   I want to view my profile page
   So that I can see my account information) do
   scenario 'view account information' do
-  user = FactoryBot.create(:user)
+    user = FactoryBot.create(:user)
 
-   visit user_path(user)
+    visit user_path(user)
 
-   expect(page).to have_content(user.username)
-   expect(page).to have_content(user.email)
-   expect(page).to have_content(user.profile_photo)
+    expect(page).to have_content(user.username)
+    expect(page).to have_content(user.email)
+    expect(page).to have_content(user.profile_photo)
   end
 end
